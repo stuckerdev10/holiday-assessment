45cad9b0ae0aa4d96fca8f934fec9f25006b08e4
Sawyer Tucker <stucker@dev-10.com>
2022-01-10 14:05:00 -0600

    added menu main and fixed bugs

--- holiday-assessment.py
+++ holiday-assessment.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[200]:
+# In[669]:
 
 
 import datetime 
@@ -11,9 +11,10 @@ import json
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+from pprint import pprint
 
 
-# In[207]:
+# In[670]:
 
 
 # -------------------------------------------
@@ -29,44 +30,12 @@ class Holiday:
     date: datetime.datetime
     
     def __str__ (self):
-        return f"{self.name}: {self.date.date()}"
+        return f"{self.name}: {self.date}"
         # String output
         # Holiday output when printed.
 
 
-# In[208]:
-
-
-test_examples = Holiday("Festivus", datetime.datetime(2022, 12, 21))
-print(test_examples)
-
-
-# In[113]:
-
-
-testlist = HolidayList()
-
-
-# In[114]:
-
-
-for test in test_examples:
-    testlist.addHoliday(test)
-
-
-# In[194]:
-
-
-testlist.innerHolidays
-
-
-# In[195]:
-
-
-type(testlist.innerHolidays[0].date)
-
-
-# In[413]:
+# In[716]:
 
 
 # -------------------------------------------
@@ -103,6 +72,13 @@ class HolidayList:
             return 11
         elif month == 'Dec':
             return 12
+    def strToDate(self, date):
+        try:
+            split_str = date.split('-')
+            cast_date = datetime.datetime(int(split_str[0]), int(split_str[1]), int(split_str[2]))
+            return cast_date
+        except:
+            print('Incorrect formatting, please try again.')
         
     def addHoliday(self, holidayObj):
         if isinstance(holidayObj, Holiday):
@@ -123,7 +99,7 @@ class HolidayList:
         # Return Holiday
     def removeHoliday(self, HolidayName, Date):
         for holiday in self.innerHolidays:
-            if HolidayName == holiday.name and Date == datetime.ToShortDateString(holiday.date):
+            if HolidayName == holiday.name and Date == holiday.date.date():
                 self.innerHolidays.remove(self.findHoliday(HolidayName, Date))
                 print(f"The holiday {HolidayName} was removed")
         
@@ -131,10 +107,12 @@ class HolidayList:
         # remove the Holiday from innerHolidays
         # inform user you deleted the holiday
     def read_json(self):
+        print("reading json file...")
+        time.sleep(1)
         with open('holidays.json', 'r') as file:
             holiday_json = json.load(file)
             for holiday in holiday_json['holidays']:
-                holiday_entry = Holiday(holiday['name'], holiday['date'])
+                holiday_entry = Holiday(holiday['name'], self.strToDate(holiday['date'].split(' ')[0]))
                 self.addHoliday(holiday_entry)
             file.close()
 
@@ -142,18 +120,22 @@ class HolidayList:
         # Read in things from json file location
         # Use addHoliday function to add holidays to inner list.
     def save_to_json(self):
+        print("Overwriting data...")
+        time.sleep(1)
         data = {}
         data['holidays'] = []
         for holiday in self.innerHolidays:
-            data['holidays'].append({'name': holiday.name,                                     'date': holiday.date})
+            data['holidays'].append({'name': holiday.name,                                     'date': str(holiday.date)})
 
-        with open('holidays.json', 'rw') as file:
+        with open('holidays.json', 'w') as file:
             file.seek(0)
             file.truncate()
             json.dump(data, file)
         file.close()
         print("Data has been saved")
     def scrapeHolidays(self):
+        print('Scraping holidays from web...')
+        yearlist = ['2020', '2021', '2022', '2023', '2024']
         for year in yearlist:
             time.sleep(.1)
             response = requests.get(f'https://www.timeanddate.com/calendar/custom.html?year={year}&country=1&cols=3&hol=33554809&df=1')
@@ -204,25 +186,118 @@ class HolidayList:
         hol_list = self.filter_holidays_by_week(year, weekNum)
         for h in hol_list:
             print(h)
-    def getWeather():
+    def getWeather(self):
+        url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
+
+        querystring = {"q":"Minneapolis","days":"5"}
+
+        headers = {
+            'x-rapidapi-host': "weatherapi-com.p.rapidapi.com",
+            'x-rapidapi-key': "d6018c029fmsh69bf22a36ce922bp1891b1jsn6a958d456fac"
+            }
+
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        weather = response.json()
+
+        for day in weather['forecast']['forecastday']:
+            date_split = day['date'].split('-')
+            cast_date = datetime.datetime(int(date_split[0]), int(date_split[1]), int(date_split[2]))
+            condition = day['day']['condition']['text']
+            holidays = list(filter(lambda x: x.date == cast_date, self.innerHolidays))
+            print(f"{cast_date.date()}: {holidays}: {condition}")
+        
+    def viewCurrentWeek(self):
+        week_list = []
+        a_time = datetime.datetime.today()
+        start_time = a_time.replace(hour=0, minute=0, second=0, microsecond=0)
+        week_list.append(start_time)
+        for i in range(1, 7):
+            new_time = start_time + datetime.timedelta(days = i)
+            week_list.append(new_time)
+        for day in week_list:
+            holidays = list(filter(lambda x: x.date == day, self.innerHolidays))
+            if len(holidays) == 0:
+                print(f"{day.date()}: None")
+            else:    
+                print(f"{day.date()}: {holidays[0].name}")
+        print("Would you like to view the weather forecast? (Y/N)")
+        weather_choice = input()
+        if weather_choice == 'Y':
+            self.getWeather()
         
         
     
 
 
-# In[415]:
-
-
-
-print(newlist.getWeather())
-print(datetime.datetime(2022, 1, 10))
-#rint(newlist.getWeather() == datetime.datetime(2022, 1, 10))
-
-
-# In[ ]:
+# In[725]:
 
 
 def main():
+    print('--------------------------------------')
+    print('       Welcome to Holiday Creator     ')
+    print('--------------------------------------')
+    newlist = HolidayList()
+    newlist.read_json()
+    #newlist.scrapeHolidays()
+    menu = True
+    while menu == True:
+        print(f"There are {newlist.numHolidays()} holidays in the system")
+        print('--------------------------------------')
+        print('        Holiday Creator Main Menu     ')
+        print('--------------------------------------')
+        print('Select an option:')
+        print('1. Add a Holiday')
+        print('2. Remove a Holiday')
+        print('3. Save Holiday List')
+        print('4. View Holidays')
+        print('5. Exit Program')
+        choice = input()
+        
+        if choice =='1':
+            print('Enter the name of the holiday')
+            name = input()
+            print('Enter the date of the holiday in YYYY-mm-dd format')
+            date_str = input()
+            date = newlist.strToDate(date_str)
+            new_holiday = Holiday(name, date)
+            newlist.addHoliday(new_holiday)
+            
+        if choice == '2':
+            print('Enter the name of the Holiday')
+            name = input()
+            print('Enter the date of the holiday in YYYY-mm-dd format')
+            date_str = input()
+            date = newlist.strToDate(date_str)
+            newlist.removeHoliday(name, date)
+            
+        if choice == '3':
+            print('Are you sure you want to save your changes? (Y/N)')
+            yn = input()
+            if yn == 'Y':
+                newlist.save_to_json()
+        
+        if choice == '4':
+            print('Choose the year you want to view')
+            year = int(input())
+            print('Choose the week number you want to view')
+            week = int(input())
+            print(newlist.display_holidays_by_week(year, week))
+            time.sleep(1)
+            print('Would you like to see the forecast? (Y/N)')
+            choice2 = input()
+            if choice2 == 'Y':
+                newlist.getWeather()
+                
+        if choice == '5':
+            print('Are you sure you want to quit? Any unsaved progress will be lost.')
+            yn = input()
+            if yn == 'Y':
+                menu = False
+            
+            
+            
+            
+        
     # Large Pseudo Code steps
     # -------------------------------------
     # 1. Initialize HolidayList Object
@@ -235,12 +310,13 @@ def main():
     # 7. Ask the User if they would like to Continue, if not, end the while loop, ending the program.  If they do wish to continue, keep the program going. 
 
 
-# In[ ]:
+# In[586]:
 
 
 if __name__ == "__main__":
     main();
-
+    
+    
 
 # Additional Hints:
 # ---------------------------------------------
@@ -257,4 +333,10 @@ if __name__ == "__main__":
 # and substitute the placeholders 
 # for example: filetxt.format(fname = "John", age = 36)
 # This will make your code far more readable, by seperating text from code.
+
+
+# In[ ]:
+
+
+
 
